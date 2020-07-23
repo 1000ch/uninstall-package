07e7172f258208ec37defa9aefd9ae31d842bb33
1000ch <shogosensui@gmail.com>
2020-07-24 00:24:59 +0900

    Fix test case

--- test/index.js
+++ test/index.js
@@ -7,8 +7,8 @@ const uninstall = require('../');
 test('uninstall a package', t => {
   t.plan(2);
   return Promise.resolve()
-    .then(() => install('co'))
-    .then(() => uninstall('co'))
+    .then(() => install('co', ['--no-save']))
+    .then(() => uninstall('co', ['--no-save']))
     .then(result => {
       t.equal(result.error, null);
       t.ok(result.stdout || result.stderr);
@@ -18,8 +18,8 @@ test('uninstall a package', t => {
 test('uninstall multiple packages', t => {
   t.plan(2);
   return Promise.resolve()
-    .then(() => install(['co', 'co-fs']))
-    .then(() => uninstall(['co', 'co-fs']))
+    .then(() => install(['co', 'co-fs'], ['--no-save']))
+    .then(() => uninstall(['co', 'co-fs'], ['--no-save']))
     .then(result => {
       t.equal(result.error, null);
       t.ok(result.stdout || result.stderr);
@@ -29,19 +29,19 @@ test('uninstall multiple packages', t => {
 test('uninstall not exist package', t => {
   t.plan(2);
   return Promise.resolve()
-    .then(() => install(['co', 'co-fs']))
-    .then(() => uninstall('not-exist-package'))
+    .then(() => install(['co', 'co-fs'], ['--no-save']))
+    .then(() => uninstall('not-exist-package', ['--no-save']))
     .then(result => {
       t.equal(result.error, null);
-      t.ok(!result.stdout && !result.stderr);
+      t.ok(result.stdout || result.stderr);
     });
 });
 
 test('accept string options', t => {
   t.plan(2);
   return Promise.resolve()
-    .then(() => install(['co', 'co-fs']))
-    .then(() => uninstall('co', '--save'))
+    .then(() => install(['co', 'co-fs'], ['--no-save']))
+    .then(() => uninstall('co', '--no-save', ['--no-save']))
     .then(result => {
       t.equal(result.error, null);
       t.ok(result.stdout || result.stderr);
@@ -51,8 +51,8 @@ test('accept string options', t => {
 test('accept array options', t => {
   t.plan(2);
   return Promise.resolve()
-    .then(() => install(['co', 'co-fs']))
-    .then(() => uninstall('co', ['--save']))
+    .then(() => install(['co', 'co-fs'], ['--no-save']))
+    .then(() => uninstall('co', ['--no-save']))
     .then(result => {
       t.equal(result.error, null);
       t.ok(result.stdout || result.stderr);
@@ -62,8 +62,8 @@ test('accept array options', t => {
 test('accept object options', t => {
   t.plan(2);
   return Promise.resolve()
-    .then(() => install(['co', 'co-fs']))
-    .then(() => uninstall('co', { '--save' : true }))
+    .then(() => install(['co', 'co-fs'], ['--no-save']))
+    .then(() => uninstall('co', { '--no-save' : true }))
     .then(result => {
       t.equal(result.error, null);
       t.ok(result.stdout || result.stderr);
