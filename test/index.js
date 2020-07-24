610b14ec5d175315b2039bc717078ef0920ee279
1000ch <shogosensui@gmail.com>
2020-07-24 13:17:16 +0900

    Fix tests

--- test/index.js
+++ test/index.js
@@ -3,44 +3,74 @@ const test = require('ava');
 const install = require('install-package');
 const uninstall = require('..');
 
-test('uninstall a package', async t => {
-  await install('noop2', ['--no-save']);
-  const {stdout} = await uninstall('noop2', ['--no-save']);
+test.beforeEach(async () => {
+  await install(['noop2', 'noop3'], '--no-save');
+});
 
-  t.not(stdout, null);
+test('uninstall a package', async t => {
+  try {
+    const {stdout} = await uninstall('noop2', '--no-save');
+
+    t.not(stdout, null);
+  } catch (error) {
+    t.fail(error);
+  }
 });
 
 test('uninstall multiple packages', async t => {
-  await install(['noop2', 'noop3'], ['--no-save']);
-  const {stdout} = await uninstall(['noop2', 'noop3'], ['--no-save']);
+  try {
+    const {stdout} = await uninstall(['noop2', 'noop3'], '--no-save');
 
-  t.not(stdout, null);
+    t.not(stdout, null);
+  } catch (error) {
+    t.fail(error);
+  }
 });
 
 test('uninstall not exist package', async t => {
-  await install(['noop2', 'noop3'], ['--no-save']);
-  const {stdout} = await uninstall('not-exist-package', ['--no-save']);
+  try {
+    const {stdout} = await uninstall('not-exist-package', '--no-save');
 
-  t.not(stdout, null);
+    t.not(stdout, null);
+  } catch (error) {
+    t.fail(error);
+  }
+});
+
+test('throw exception when package is blank', async t => {
+  try {
+    await uninstall(null);
+  } catch (error) {
+    t.not(error, null);
+  }
 });
 
 test('accept string options', async t => {
-  await install(['noop2', 'noop3'], ['--no-save']);
-  const {stdout} = await uninstall('noop2', '--no-save');
+  try {
+    const {stdout} = await uninstall('noop2', '--no-save');
 
-  t.not(stdout, null);
+    t.not(stdout, null);
+  } catch (error) {
+    t.fail(error);
+  }
 });
 
 test('accept array options', async t => {
-  await install(['noop2', 'noop3'], ['--no-save']);
-  const {stdout} = await uninstall('noop2', ['--no-save']);
+  try {
+    const {stdout} = await uninstall('noop2', ['--no-save']);
 
-  t.not(stdout, null);
+    t.not(stdout, null);
+  } catch (error) {
+    t.fail(error);
+  }
 });
 
 test('accept object options', async t => {
-  await install(['noop2', 'noop3'], ['--no-save']);
-  const {stdout} = await uninstall('noop2', {'--no-save': true});
+  try {
+    const {stdout} = await uninstall('noop2', {'--no-save': true});
 
-  t.not(stdout, null);
+    t.not(stdout, null);
+  } catch (error) {
+    t.fail(error);
+  }
 });
